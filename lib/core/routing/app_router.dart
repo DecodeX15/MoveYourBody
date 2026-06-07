2b81986f2fef28e3786616e15a533ca590010912
Vaibhav Aryan <aryanvaibhav74@gmail.com>
2026-06-08 04:32:15 +0530

    fixed filenames and added routes_files

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-
+import './app_routes.dart';
 import '../../features/onboarding/view/body_region_screen.dart';
 import '../../features/onboarding/view/difficulty_level.dart';
 import '../../features/onboarding/view/equipment_screen.dart';
@@ -11,47 +11,44 @@ import '../../features/onboarding/view/user_details.dart';
 import '../../features/onboarding/view/welcome_screen.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: AppRoutes.welcome,
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const WelcomeScreen()),
-
     GoRoute(
-      path: '/onboarding/goals',
+      path: AppRoutes.welcome,
+      builder: (context, state) => const WelcomeScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.goals,
       builder: (context, state) => const GoalScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/health-issues',
-      builder: (context, state) => const HealthIssuesScreen(),
+      path: AppRoutes.healthIssues,
+      builder: (context, state) =>
+          const HealthIssuesScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/difficulty',
+      path: AppRoutes.difficulty,
       builder: (context, state) => const DifficultyScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/intensity',
+      path: AppRoutes.intensity,
       builder: (context, state) => const IntensityScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/target-body-region',
+      path: AppRoutes.targetBodyRegion,
       builder: (context, state) => const BodyRegionScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/equipments',
+      path: AppRoutes.equipments,
       builder: (context, state) => const EquipmentScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/userdata',
-      builder: (context, state) => const UserdataScreen(),
+      path: AppRoutes.userData,
+      builder: (context, state) =>
+          const UserdataScreen(),
     ),
-
     GoRoute(
-      path: '/onboarding/result',
+      path: AppRoutes.result,
       builder: (context, state) => const ResultScreen(),
     ),
   ],
---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,11 @@
+class AppRoutes {
+  static const String welcome = '/';
+  static const String goals = '/onboarding/goals';
+  static const String healthIssues = '/onboarding/health-issues';
+  static const String difficulty = '/onboarding/difficulty';
+  static const String intensity = '/onboarding/intensity';
+  static const String targetBodyRegion = '/onboarding/target-body-region';
+  static const String equipments = '/onboarding/equipments';
+  static const String userData = '/onboarding/userdata';
+  static const String result = '/onboarding/result';
+}
