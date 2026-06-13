08507a1b6e311e770362ab42ce13d5a02af5504b
Vaibhav Aryan <aryanvaibhav74@gmail.com>
2026-06-14 00:26:49 +0530

    adding build yaml

--- lib/features/onboarding/view_model/generated/onboarding_view_model.g.dart
+++ lib/features/onboarding/view_model/generated/onboarding_view_model.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'onboarding_view_model.dart';
+part of '../onboarding_view_model.dart';
 
 // **************************************************************************
 // RiverpodGenerator
